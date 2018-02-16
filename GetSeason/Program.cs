9746dd4490b1ed27bc4644a3c495df62d116e78a
Michael Robinson <michael.robinson@westernsouthernlife.com>
2018-02-16 11:53:27 -0500

    Initial Commit

---
+++ GetSeason/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSeason
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetSeasonClassLibrary.GetSeasonClass currentSeason = new GetSeasonClassLibrary.GetSeasonClass();
+
+            string season = currentSeason.GetSeason();
+
+            Console.WriteLine(season);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GetSeasonClassLibrary/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSeasonClassLibrary
+{
+    public class GetSeasonClass
+    {
+       public string GetSeason()
+        {
+            return "Late Winter";
+        }
+    }
+}
